59e89ae3f39c1a4793ea9654108a50960af39f0b
natata-tutorial <natata.politko@gmail.com>
2023-04-03 17:38:24 +0300

    refactoring functions

--- js/script.js
+++ js/script.js
@@ -2,63 +2,63 @@
 
 import contentObj from "./content.js";
 
+const burgerButton = document.querySelector(".burger__button");
+const sideNav = document.querySelector(".sidenav");
+const list = document.querySelector(".sidenav__list");
+const contentSection = document.querySelector(".content__seciton");
+
 document.addEventListener("DOMContentLoaded", createSidebarList);
 
 function createSidebarList() {
-   const sideNav = document.querySelector(".sidenav");
-   const list = document.createElement("ul");
-   list.classList.add("sidenav__list");
+   if (list && burgerButton && contentSection) {
+      const listHtml = contentObj
+         .map(({ id, styleTitle }) => {
+            return `<li>
+            <a href="#" class="sidenav__item" 
+            id="${id}" > ${styleTitle}
+            </a>
+            </li>`;
+         })
+         .join("");
 
-   const listHtml = contentObj
-      .map((item) => {
-         const { id, styleTitle } = item;
-         return `<li>
-         <a href="#" class="sidenav__item" 
-         id="${id}" > ${styleTitle}
-         </a>
-         </li>`;
-      })
-      .join("");
-   list.innerHTML = listHtml;
-   sideNav.append(list);
-   sideNav.addEventListener("click", handleSidebarItemClick);
+      list.innerHTML = listHtml;
+
+      sideNav.addEventListener("click", handleSidebarItemClick);
+   }
 }
 
 function handleSidebarItemClick(event) {
-
-   const burgerButton = document.querySelector(".burger__button");
    const currentItem = event.target.closest(".sidenav__item");
 
    if (currentItem) {
       if (!currentItem.classList.contains("active")) {
          const activeItem = this.querySelector(".active");
-         if (activeItem) activeItem.classList.remove("active");
-         currentItem.classList.add("active");
-         displayContent(currentItem.id);
-      }
 
-      burgerButton.click();
+         if (activeItem) activeItem.classList.remove("active");
+
+         currentItem.classList.add("active");
+         const newContent = contentObj.find((el) => el.id === currentItem.id);
+
+         if (newContent) {
+            renderContent(newContent);
+         }
+      }
    }
+
+   burgerButton.click();
 }
 
-function displayContent(contentId) {
+function renderContent(newContent) {
+   const { styleTitle, stylePicture, styleDescription } = newContent;
 
-   const newContent = contentObj.find((el) => el.id === contentId);
-   if (newContent) {
-
-      const contentSection = document.querySelector(".content__seciton");
-
-      const { styleTitle, stylePicture, styleDescription } = newContent;
-
-      contentSection.innerHTML = `
-         <h2 class="content__title">${styleTitle}</h2>
-         <img
-            class="content__picture"
-            src="${stylePicture}"
-            alt="${styleTitle}"
-            />
-         <div class="content__text">
-         ${styleDescription}
-         </div>`;
-   }
+   contentSection.innerHTML = `
+      <h2 class="content__title">${styleTitle}</h2>
+      <img
+         class="content__picture"
+         src="${stylePicture}"
+         alt="${styleTitle}"
+         />
+      <div class="content__text">
+      ${styleDescription}
+      </div>`;
 }
